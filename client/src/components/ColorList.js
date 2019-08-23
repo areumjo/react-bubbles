0daec6ab99071a4ecd82366efe975d37ec8189c1
Areum Jo <areumjo1@gmail.com>
2019-08-23 09:56:14 -0700

    MVP Completed: saveEdit, deleteColor working now

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const initialColor = {
   color: "",
@@ -7,7 +7,7 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  //console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -18,13 +18,28 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
+    console.log('colorToEdit state: ', colorToEdit, 'id: ', colorToEdit.id)
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => console.log(res))
+      .catch(err => console.log(err.response))
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    console.log('color: ', color, 'id: ', color.id)
+
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(res => {
+        console.log('delete-data:', res)
+        const newColorList = colors.filter(c => c.id != color.id)
+        updateColors(newColorList)
+      })
+      .catch(err => console.log(err))
   };
 
   return (
